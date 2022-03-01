2c401fb752a67e1fcfbc77dde6d560d0e4bdc83f
yordi <yyordi94@gmail.com>
2022-02-28 23:12:04 -0500

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@ let letters = 'Supercalifragilisticexpialidocious';
 console.log(letters.length)
 let BigInt = '12344556666678999888n'
 
-
+console.log(BigInt);
 console.log(4)
-console.log(BigInt)
 console.log(typeof 4)
 console.log(typeof BigInt)
+console.log(80 + "71.2")
+console.log("hello world")
