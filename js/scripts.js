00d5f7b4c88eb4a65f7d0a7261dc960c328bdecd
yordi <yyordi94@gmail.com>
2022-02-28 22:00:03 -0500

    adding files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let letters = 'Supercalifragilisticexpialidocious';
+console.log(letters.length)
+let BigInt = '12344556666678999888n'
+
+
+console.log(4)
+console.log(BigInt)
+console.log(typeof 4)
+console.log(typeof BigInt)
